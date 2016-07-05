772a06560ac6e85afbcf78b4a9d3cbf2d324cb9b
Felipe Lima <felipe.lima@gmail.com>
2016-07-05 10:59:40 -0700

    Update and fix the sample code
    Make it work with latest jsaddle (0.4.0.4)

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,12 @@ module Main (
 
 import GHCJS.DOM (runWebGUI, postGUIAsync)
 import Control.Lens ((^.))
-import Language.Javascript.JSaddle (jsg, js, (<#), runJSaddle_)
+import Language.Javascript.JSaddle (jsg, js, jss, runJSaddle)
 
 main = runWebGUI $ \ webView -> do
-    let runjs = postGUIAsync . runJSaddle_ webView
-
+    let runjs = postGUIAsync . runJSaddle webView
     runjs $ do
         document <- jsg "document"
         let body = js "body"
-            innerHtml = js "innerHtml"
-        document ^. body ^. innerHtml <# "<h1>Hello World</h1>"
-
+            innerHtml = jss "innerHTML"
+        document ^. body ^. innerHtml "<h1>Hello World</h1>"
